38a274a730f3e8e274a30f192eb2bc137f057caa
matt-stanford <mjfstanford@gmail.com>
2020-07-11 14:20:20 +0100

    Added Order and OrderItem models

--- store/models.py
+++ store/models.py
@@ -41,10 +41,6 @@ class Product(models.Model):
     def get_url(self):
         return reverse('product_detail', args=[self.category.slug, self.slug])
 
-    @property
-    def monthly(self):
-        return self.price // 12
-
     def __str__(self):
         return self.name
 
@@ -75,6 +71,48 @@ class CartItem(models.Model):
 
     def sub_total(self):
         return self.product.price * self.quantity
+
+    def __str__(self):
+        return self.product
+
+    objects = models.Manager()
+
+class Order(models.Model):
+    token = models.CharField(max_length=250, blank=True)
+    total = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='USD Order Total')
+    emailAddress = models.EmailField(max_length=250, blank=True, verbose_name='Email Address')
+    created = models.DateTimeField(auto_now_add=True)
+    billingName = models.CharField(max_length=250, blank=True)
+    billingAddress1 = models.CharField(max_length=250, blank=True)
+    billingCity = models.CharField(max_length=250, blank=True)
+    billingPostcode = models.CharField(max_length=250, blank=True)
+    billingCountry = models.CharField(max_length=250, blank=True)
+    shippingName = models.CharField(max_length=250, blank=True)
+    shippingAddress1 = models.CharField(max_length=250, blank=True)
+    shippingCity = models.CharField(max_length=250, blank=True)
+    shippingPostcode = models.CharField(max_length=250, blank=True)
+    shippingCountry = models.CharField(max_length=250, blank=True)
+
+    class Meta:
+        db_table = 'Order'
+        ordering = ['-created']
+
+    def __str__(self):
+        return str(self.id)
+
+    objects = models.Manager()
+
+class OrderItem(models.Model):
+    product = models.CharField(max_length=250)
+    quantity = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='USD Price')
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'OrderItem'
+
+    def sub_total(self):
+        return self.quantity * self.price
 
     def __str__(self):
         return self.product
