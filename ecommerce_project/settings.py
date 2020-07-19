ff319024013e6b4a067d1f6bd84735b2fb7b310e
matt-stanford <mjfstanford@gmail.com>
2020-07-19 21:04:02 +0100

    Updated settings.py AWS settings

--- ecommerce_project/settings.py
+++ ecommerce_project/settings.py
@@ -137,9 +137,8 @@ EMAIL_USE_TLS = True
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AUDIO24_AWS_STORAGE_BUCKET_NAME')
-
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
-DEFAUL_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 django_heroku.settings(locals())
